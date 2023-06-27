f0afd4359dcdb955e8f8065f9268effa3559c22e
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 12:06:05 -0700

    this is the answer of reverse-words

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function reverseWords(sentence) {
+ 
+    const words = sentence.split(" ");
+    
+    const reversedWords = words.reverse();
+    
+    const reversedSentence = reversedWords.join(" ");
+    
+    return reversedSentence;
+  }
+  
+  console.log(reverseWords("fikrcamp bootcamp")); 
+  console.log(reverseWords("hello there!")); 
+  console.log(reverseWords("welcome to my city")); 
